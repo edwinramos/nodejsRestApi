ec58a84a560e019ddfca135ff002c18842976d43
Edwin Ramos <edwinramos.93@gmail.com>
2016-12-08 14:53:36 +0000

    adapted tu contacts

--- server.js
+++ server.js
@@ -6,40 +6,32 @@ var router =  express.Router();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://dbaccess:dbaccess@ds119608.mlab.com:19608/heroku_n08k67hr');
 
-var Bear = require('./app/models/bear');
+var Contact = require('./app/models/contact');
 
 router.get('/', function(req, res){
     res.json({message: 'HOLA MUNDO'});
 });
 
-router.post('/bears', function(req, res){
-    var bear = new Bear();
-    bear.name = req.body.name;
+router.post('/contacts', function(req, res){
+    var contact = new Contact();
+    contact.name = req.body.Name;
+    contact.lastName = req.body.LastName;
 
-    bear.save(function(err){
+    contact.save(function(err){
         if(err)
             res.send(err);
             
-        res.json({message: 'Bear created'});
+        res.json({message: 'Contact created'});
     });
 });
 
-router.get('/bears', function(req, res){
-    Bear.find(function(err, result){
-        if(err)
-            res.send(err);
-            
-        res.json(result);
-    });
-});
-
-router.get('/bears/:_id', function(req, res){
-    Bear.find({_id: req.params['_id']}, function(err, result){
+router.get('/contacts', function(req, res){
+    Contact.find(function(err, result){
         if(err)
             res.send(err);
             
@@ -47,22 +39,31 @@ router.get('/bears/:_id', function(req, res){
     });
 });
 
-router.put('/bears/:_id', function(req, res){
-    Bear.update({_id: req.params['_id']}, req.body, function (err, result) {
-        if (err) {
+router.get('/contacts/:_id', function(req, res){
+    Contact.find({_id: req.params['_id']}, function(err, result){
+        if(err)
             res.send(err);
-        } 
-        res.json({message: 'Bear updated'});
+            
+        res.json(result);
     });
 });
 
-router.delete('/bears/:_id', function(req, res){
-
-    Bear.remove({_id: req.params['_id']}, function (err, result) {
+router.put('/contacts/:_id', function(req, res){
+    Contact.update({_id: req.params['_id']}, req.body, function (err, result) {
         if (err) {
             res.send(err);
         } 
-        res.json({message: 'Bear deleted'});
+        res.json({message: 'Contact updated'});
+    });
+});
+
+router.delete('/contacts/:_id', function(req, res){
+
+    Contact.remove({_id: req.params['_id']}, function (err, result) {
+        if (err) {
+            res.send(err);
+        } 
+        res.json({message: 'Contact deleted'});
     });
 });
 
